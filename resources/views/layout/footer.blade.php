1152fb084d0d5177ace22984d0e3e687c5da0b69
Razan-Rjoub <ralrjoub6@gmail.com>
2023-11-13 21:56:09 +0300

    Razan

---
+++ resources/views/layout/footer.blade.php
@@ -0,0 +1,268 @@
+
+<!--Site Footer Start-->
+<footer class="site-footer">
+    <div class="site-footer__img">
+        <img src="assets/images/resources/site-footer-img.jpg" alt="">
+    </div>
+    <div class="container">
+        <div class="site-footer__top">
+            <div class="footer-widget__logo">
+                <a href="index.html"><img src="assets/images/resources/footer-logo.png" alt=""></a>
+            </div>
+            <div class="footer-widget__subscribe-box">
+                <div class="footer-widget__subscribe-text">
+                    <p>Subscribe to Newsletter</p>
+                </div>
+                <form class="footer-widget__email-box mc-form" data-url="#">
+                    <div class="footer-widget__email-input-box">
+                        <input type="email" placeholder="Email Address" name="EMAIL">
+                    </div>
+                    <button type="submit" class="footer-widget__subscribe-btn thm-btn">Subscribe</button>
+                </form>
+                <div class="mc-form__response"></div><!-- /.mc-form__response -->
+            </div>
+        </div>
+        <div class="site-footer__middle">
+            <div class="row">
+                <div class="col-xl-3 col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="100ms">
+                    <div class="footer-widget__column footer-widget__Contact">
+                        <div class="footer-widget__title-box">
+                            <h3 class="footer-widget__title">Contact</h3>
+                        </div>
+                        <p class="footer-widget__Contact-text">80 Road Broklyn Street, 600 <br> New York, USA
+                        </p>
+                        <ul class="footer-widget__Contact-list list-unstyled">
+                            <li>
+                                <div class="icon">
+                                    <span class="icon-email"></span>
+                                </div>
+                                <div class="text">
+                                    <p><a href="mailto:needhelp@company.com">needhelp@company.com</a></p>
+                                </div>
+                            </li>
+                            <li>
+                                <div class="icon">
+                                    <span class="fas fa-phone-square"></span>
+                                </div>
+                                <div class="text">
+                                    <p><a href="tel:+926668880000">+92 666 888 0000</a></p>
+                                </div>
+                            </li>
+                        </ul>
+                        <div class="site-footer__social">
+                            <a href="#"><i class="fab fa-twitter"></i></a>
+                            <a href="#"><i class="fab fa-facebook"></i></a>
+                            <a href="#"><i class="fab fa-pinterest-p"></i></a>
+                            <a href="#"><i class="fab fa-instagram"></i></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-2 col-lg-2 col-md-6 wow fadeInUp" data-wow-delay="200ms">
+                    <div class="footer-widget__column footer-widget__link">
+                        <div class="footer-widget__title-box">
+                            <h3 class="footer-widget__title">Links</h3>
+                        </div>
+                        <ul class="footer-widget__link-list list-unstyled">
+                            <li><a href="about.html">About</a></li>
+                            <li><a href="team.html">Our Team</a></li>
+                            <li><a href="events.html">Upcoming Events</a></li>
+                            <li><a href="news.html">Latest News</a></li>
+                            <li><a href="contact.html">Contact</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-xl-2 col-lg-3 col-md-6 wow fadeInUp" data-wow-delay="300ms">
+                    <div class="footer-widget__column footer-widget__departments">
+                        <div class="footer-widget__title-box">
+                            <h3 class="footer-widget__title">Departments</h3>
+                        </div>
+                        <ul class="footer-widget__link-list list-unstyled">
+                            <li><a href="about.html">Health & Safety</a></li>
+                            <li><a href="about.html">Housing & Land</a></li>
+                            <li><a href="about.html">Legal & Finance</a></li>
+                            <li><a href="about.html">Transport & Traffic</a></li>
+                            <li><a href="about.html">Arts & Culture</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-xl-2 col-lg-3 col-md-6 wow fadeInUp" data-wow-delay="400ms">
+                    <div class="footer-widget__column footer-widget__explore">
+                        <div class="footer-widget__title-box">
+                            <h3 class="footer-widget__title">Explore</h3>
+                        </div>
+                        <ul class="footer-widget__link-list list-unstyled">
+                            <li><a href="about.html">Administration</a></li>
+                            <li><a href="about.html">Fire Services</a></li>
+                            <li><a href="tax-return.html">Business & Taxation</a></li>
+                            <li><a href="about.html">Circular’s And Go’s</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-xl-3 col-lg-6 col-md-6 wow fadeInUp" data-wow-delay="500ms">
+                    <div class="footer-widget__column footer-widget__gallery">
+                        <div class="footer-widget__title-box">
+                            <h3 class="footer-widget__title">Gallery</h3>
+                        </div>
+                        <ul class="footer-widget__gallery-list list-unstyled clearfix">
+                            <li>
+                                <div class="footer-widget__gallery-img">
+                                    <img src="assets/images/gallery/footer-widget-gallery-img-1.jpg" alt="">
+                                    <a href="assets/images/gallery/footer-widget-gallery-img-1.jpg"
+                                        class="img-popup"><span class="fab fa-instagram"></span></a>
+                                </div>
+                            </li>
+                            <li>
+                                <div class="footer-widget__gallery-img">
+                                    <img src="assets/images/gallery/footer-widget-gallery-img-2.jpg" alt="">
+                                    <a href="assets/images/gallery/footer-widget-gallery-img-2.jpg"
+                                        class="img-popup"><span class="fab fa-instagram"></span></a>
+                                </div>
+                            </li>
+                            <li>
+                                <div class="footer-widget__gallery-img">
+                                    <img src="assets/images/gallery/footer-widget-gallery-img-3.jpg" alt="">
+                                    <a href="assets/images/gallery/footer-widget-gallery-img-3.jpg"
+                                        class="img-popup"><span class="fab fa-instagram"></span></a>
+                                </div>
+                            </li>
+                            <li>
+                                <div class="footer-widget__gallery-img">
+                                    <img src="assets/images/gallery/footer-widget-gallery-img-4.jpg" alt="">
+                                    <a href="assets/images/gallery/footer-widget-gallery-img-4.jpg"
+                                        class="img-popup"><span class="fab fa-instagram"></span></a>
+                                </div>
+                            </li>
+                            <li>
+                                <div class="footer-widget__gallery-img">
+                                    <img src="assets/images/gallery/footer-widget-gallery-img-5.jpg" alt="">
+                                    <a href="assets/images/gallery/footer-widget-gallery-img-5.jpg"
+                                        class="img-popup"><span class="fab fa-instagram"></span></a>
+                                </div>
+                            </li>
+                            <li>
+                                <div class="footer-widget__gallery-img">
+                                    <img src="assets/images/gallery/footer-widget-gallery-img-6.jpg" alt="">
+                                    <a href="assets/images/gallery/footer-widget-gallery-img-6.jpg"
+                                        class="img-popup"><span class="fab fa-instagram"></span></a>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="site-footer__bottom">
+        <div class="container">
+            <div class="row">
+                <div class="col-xl-12">
+                    <div class="site-footer__bottom-inner">
+                        <p class="site-footer__bottom-text">© Copyright 2023 by <a href="#">Govity Template</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+<!--Site Footer End-->
+
+
+</div><!-- /.page-wrapper -->
+
+
+<div class="mobile-nav__wrapper">
+<div class="mobile-nav__overlay mobile-nav__toggler"></div>
+<!-- /.mobile-nav__overlay -->
+<div class="mobile-nav__content">
+    <span class="mobile-nav__close mobile-nav__toggler"><i class="fa fa-times"></i></span>
+
+    <div class="logo-box">
+        <a href="index.html" aria-label="logo image"><img src="assets/images/resources/logo-1.png" width="94"
+                alt="" /></a>
+    </div>
+    <!-- /.logo-box -->
+    <div class="mobile-nav__container"></div>
+    <!-- /.mobile-nav__container -->
+
+    <ul class="mobile-nav__contact list-unstyled">
+        <li>
+            <i class="fa fa-envelope"></i>
+            <a href="mailto:needhelp@packageName__.com">needhelp@govity.com</a>
+        </li>
+        <li>
+            <i class="fa fa-phone-alt"></i>
+            <a href="tel:666-888-0000">666 888 0000</a>
+        </li>
+    </ul><!-- /.mobile-nav__contact -->
+    <div class="mobile-nav__top">
+        <div class="mobile-nav__social">
+            <a href="#" class="fab fa-twitter"></a>
+            <a href="#" class="fab fa-facebook-square"></a>
+            <a href="#" class="fab fa-pinterest-p"></a>
+            <a href="#" class="fab fa-instagram"></a>
+        </div><!-- /.mobile-nav__social -->
+    </div><!-- /.mobile-nav__top -->
+
+
+
+</div>
+<!-- /.mobile-nav__content -->
+</div>
+<!-- /.mobile-nav__wrapper -->
+
+<div class="search-popup">
+<div class="search-popup__overlay search-toggler"></div>
+<!-- /.search-popup__overlay -->
+<div class="search-popup__content">
+    <form action="#">
+        <label for="search" class="sr-only">search here</label><!-- /.sr-only -->
+        <input type="text" id="search" placeholder="Search Here..." />
+        <button type="submit" aria-label="search submit" class="thm-btn">
+            <i class="icon-magnifying-glass"></i>
+        </button>
+    </form>
+</div>
+<!-- /.search-popup__content -->
+</div>
+<!-- /.search-popup -->
+
+<a href="#" data-target="html" class="scroll-to-target scroll-to-top"><i class="icon-right-arrow"></i></a>
+
+
+<script src="{{ asset('assets/vendors/jquery/jquery-3.6.0.min.js')}}"></script>
+<script src="{{ asset('assets/vendors/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+<script src="{{ asset('assets/vendors/jarallax/jarallax.min.js')}}"></script>
+<script src="{{ asset('assets/vendors/jquery-ajaxchimp/jquery.ajaxchimp.min.js')}}"></script>
+<script src="{{ asset('assets/vendors/jquery-appear/jquery.appear.min.js')}}"></script>
+<script src="{{ asset('assets/vendors/jquery-circle-progress/jquery.circle-progress.min.js')}}"></script>
+<script src="{{ asset('assets/vendors/jquery-magnific-popup/jquery.magnific-popup.min.js')}}"></script>
+<script src="{{ asset('assets/vendors/jquery-validate/jquery.validate.min.js')}}"></script>
+<script src="{{ asset('assets/vendors/nouislider/nouislider.min.js')}}"></script>
+<script src="{{ asset('assets/vendors/odometer/odometer.min.js')}}"></script>
+<script src="{{ asset('assets/vendors/swiper/swiper.min.js')}}"></script>
+<script src="{{ asset('assets/vendors/tiny-slider/tiny-slider.min.js')}}"></script>
+<script src="{{ asset('assets/vendors/wnumb/wNumb.min.js')}}"></script>
+<script src="{{ asset('assets/vendors/wow/wow.js')}}"></script>
+<script src="{{ asset('assets/vendors/isotope/isotope.js')}}"></script>
+<script src="{{ asset('assets/vendors/countdown/countdown.min.js')}}"></script>
+<script src="{{ asset('assets/vendors/owl-carousel/owl.carousel.min.js')}}"></script>
+<script src="{{ asset('assets/vendors/bxslider/jquery.bxslider.min.js')}}"></script>
+<script src="{{ asset('assets/vendors/bootstrap-select/js/bootstrap-select.min.js')}}"></script>
+<script src="{{ asset('assets/vendors/vegas/vegas.min.js')}}"></script>
+<script src="{{ asset('assets/vendors/jquery-ui/jquery-ui.js')}}"></script>
+<script src="{{ asset('assets/vendors/timepicker/timePicker.js')}}"></script>
+<script src="{{ asset('assets/vendors/circleType/jquery.circleType.js')}}"></script>
+<script src="{{ asset('assets/vendors/circleType/jquery.lettering.min.js')}}"></script>
+<script src="{{ asset('assets/vendors/nice-select/jquery.nice-select.min.js')}}"></script>
+
+
+
+
+<!-- template js -->
+<script src="{{ asset('assets/js/govity.js')}}"></script>
+</body>
+
+
+<!-- Mirrored from layerdrops.com/govity/main-html/index-one-page.html by HTTrack Website Copier/3.x [XR&CO'2014], Thu, 09 Nov 2023 09:16:05 GMT -->
+</html>
