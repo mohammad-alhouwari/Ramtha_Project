ed4688e65f6fa6351bf55fd80d5cdb7e0c06d9aa
MarahAbusaleh <marah.abusaleh12@gmail.com>
2023-11-14 14:42:06 +0300

    Marah

--- resources/views/layout/navbar.blade.php
+++ resources/views/layout/navbar.blade.php
@@ -3,6 +3,7 @@
 
 
 <!-- Mirrored from layerdrops.com/govity/main-html/index-one-page.html by HTTrack Website Copier/3.x [XR&CO'2014], Thu, 09 Nov 2023 09:16:05 GMT -->
+
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
@@ -23,31 +24,31 @@
         href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,wght@0,400;0,500;0,700;1,400;1,500;1,700&amp;display=swap"
         rel="stylesheet">
 
-    <link rel="stylesheet" href="{{ asset('assets/vendors/bootstrap/css/bootstrap.min.css')}}" />
-    <link rel="stylesheet" href="{{asset('assets/vendors/animate/animate.min.css')}}" />
-    <link rel="stylesheet" href="{{asset('assets/vendors/animate/custom-animate.css')}}" />
-    <link rel="stylesheet" href="{{asset('assets/vendors/fontawesome/css/all.min.css')}}" />
-    <link rel="stylesheet" href="{{asset('assets/vendors/jarallax/jarallax.css')}}" />
-    <link rel="stylesheet" href="{{asset('assets/vendors/jquery-magnific-popup/jquery.magnific-popup.css')}}" />
-    <link rel="stylesheet" href="{{asset('assets/vendors/nouislider/nouislider.min.css')}}" />
-    <link rel="stylesheet" href="{{asset('assets/vendors/nouislider/nouislider.pips.css')}}" />
-    <link rel="stylesheet" href="{{asset('assets/vendors/odometer/odometer.min.css')}}" />
-    <link rel="stylesheet" href="{{asset('assets/vendors/swiper/swiper.min.css')}}" />
-    <link rel="stylesheet" href="{{asset('assets/vendors/govity-icons/style.css')}}">
-    <link rel="stylesheet" href="{{asset('assets/vendors/tiny-slider/tiny-slider.min.css')}}" />
-    <link rel="stylesheet" href="{{asset('assets/vendors/reey-font/stylesheet.css')}}" />
-    <link rel="stylesheet" href="{{asset('assets/vendors/owl-carousel/owl.carousel.min.css')}}" />
-    <link rel="stylesheet" href="{{asset('assets/vendors/owl-carousel/owl.theme.default.min.css')}}" />
-    <link rel="stylesheet" href="{{asset('assets/vendors/bxslider/jquery.bxslider.css')}}" />
-    <link rel="stylesheet" href="{{asset('assets/vendors/bootstrap-select/css/bootstrap-select.min.css')}}" />
-    <link rel="stylesheet" href="{{asset('assets/vendors/vegas/vegas.min.css')}}" />
-    <link rel="stylesheet" href="{{asset('assets/vendors/jquery-ui/jquery-ui.css')}}" />
-    <link rel="stylesheet" href="{{asset('assets/vendors/timepicker/timePicker.css')}}" />
-    <link rel="stylesheet" href="{{asset('assets/vendors/nice-select/nice-select.css')}}" />
+    <link rel="stylesheet" href="{{ asset('assets/vendors/bootstrap/css/bootstrap.min.css') }}" />
+    <link rel="stylesheet" href="{{ asset('assets/vendors/animate/animate.min.css') }}" />
+    <link rel="stylesheet" href="{{ asset('assets/vendors/animate/custom-animate.css') }}" />
+    <link rel="stylesheet" href="{{ asset('assets/vendors/fontawesome/css/all.min.css') }}" />
+    <link rel="stylesheet" href="{{ asset('assets/vendors/jarallax/jarallax.css') }}" />
+    <link rel="stylesheet" href="{{ asset('assets/vendors/jquery-magnific-popup/jquery.magnific-popup.css') }}" />
+    <link rel="stylesheet" href="{{ asset('assets/vendors/nouislider/nouislider.min.css') }}" />
+    <link rel="stylesheet" href="{{ asset('assets/vendors/nouislider/nouislider.pips.css') }}" />
+    <link rel="stylesheet" href="{{ asset('assets/vendors/odometer/odometer.min.css') }}" />
+    <link rel="stylesheet" href="{{ asset('assets/vendors/swiper/swiper.min.css') }}" />
+    <link rel="stylesheet" href="{{ asset('assets/vendors/govity-icons/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/vendors/tiny-slider/tiny-slider.min.css') }}" />
+    <link rel="stylesheet" href="{{ asset('assets/vendors/reey-font/stylesheet.css') }}" />
+    <link rel="stylesheet" href="{{ asset('assets/vendors/owl-carousel/owl.carousel.min.css') }}" />
+    <link rel="stylesheet" href="{{ asset('assets/vendors/owl-carousel/owl.theme.default.min.css') }}" />
+    <link rel="stylesheet" href="{{ asset('assets/vendors/bxslider/jquery.bxslider.css') }}" />
+    <link rel="stylesheet" href="{{ asset('assets/vendors/bootstrap-select/css/bootstrap-select.min.css') }}" />
+    <link rel="stylesheet" href="{{ asset('assets/vendors/vegas/vegas.min.css') }}" />
+    <link rel="stylesheet" href="{{ asset('assets/vendors/jquery-ui/jquery-ui.css') }}" />
+    <link rel="stylesheet" href="{{ asset('assets/vendors/timepicker/timePicker.css') }}" />
+    <link rel="stylesheet" href="{{ asset('assets/vendors/nice-select/nice-select.css') }}" />
 
     <!-- template styles -->
-    <link rel="stylesheet" href="{{ asset('assets/css/govity.css')}}" />
-    <link rel="stylesheet" href="{{ asset('assets/css/govity-responsive.css')}}" />
+    <link rel="stylesheet" href="{{ asset('assets/css/govity.css') }}" />
+    <link rel="stylesheet" href="{{ asset('assets/css/govity-responsive.css') }}" />
 </head>
 
 <body class="custom-cursor">
@@ -241,7 +242,8 @@
                                         </div>
                                     </div>
                                     <div class="main-menu__search-box">
-                                        <a href="#" class="main-menu__search search-toggler icon-magnifying-glass"></a>
+                                        <a href="#"
+                                            class="main-menu__search search-toggler icon-magnifying-glass"></a>
                                     </div>
                                     <div class="language-switcher">
                                         <select class="selectpicker">
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,4 @@
-@extends('layout.master')
+@extends('Layout.master')
 @section('title', 'Home')
 
 @section('content')
