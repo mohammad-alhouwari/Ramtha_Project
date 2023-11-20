e3b35f87409a89a71027b0765f0d2715ef8ae6ea
MarahAbusaleh <marah.abusaleh12@gmail.com>
2023-11-20 18:07:28 +0300

    Marah 20/11

--- routes/web.php
+++ routes/web.php
@@ -5,20 +5,25 @@ use App\Http\Controllers\Frontend\ProjectController as FrontendProjectController
 use App\Http\Controllers\Frontend\NewController as FrontendNewsController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ComplainController;
+
 Route::get('/', function () {
     return view('welcome');
 });
 
-/*-----------------------------Projects Routes-----------------------------*/
+/*----------------------------- Projects Routes Start -----------------------------*/
 
 Route::controller(FrontendProjectController::class)->group(function () {
     Route::get('projects', 'showAllProjects')->name('showAllProjects');
     Route::get('project/{id}', 'showProject')->name('showProject');
 });
+/*----------------------------- Projects Routes End -----------------------------*/
+
+/*------------------------------ News Routes Start ------------------------------*/
 Route::controller(FrontendNewsController::class)->group(function () {
     Route::get('news', 'showAllNews')->name('showAllNews');
     Route::get('new-details/{id}', 'showDetailsNews')->name('showDetailsNews');
 });
+/*------------------------------- News Routes End -------------------------------*/
 
 
 Route::get('/', function () {
