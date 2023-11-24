97126e76b9e8baad690043a1304f9e36a0cbb25c
mohammad-alhouwari <mohammad.alouwari.1996@gmail.com>
2023-11-24 19:07:46 +0300

    investments

--- resources/views/Pages/Investments/investments.blade.php
+++ resources/views/Pages/Investments/investments.blade.php
@@ -55,7 +55,7 @@
                                     <div class="portfolio-page__img">
                                         <img src={{ asset($investment->image) }} alt="">
                                     </div>
-                                    <div class="portfolio-page__content">
+                                    <div class="investments-page__content">
                                         <p class="portfolio-page__sub-title">الفُرصة الإستثمارية</p>
                                         <h4 class="portfolio-page__title"><a href="{{ route('showInvestment', $investment->id) }}">{{ $investment->name }}</a></h4>
                                     </div>
