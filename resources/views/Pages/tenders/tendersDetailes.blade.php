8c86e1da52b6ffae139ee1f7740ed9dded740ce1
lamanazzal94 <lama.nazzal23@gmail.com>
2023-11-25 17:35:22 +0300

    lama

--- resources/views/Pages/tenders/tendersDetailes.blade.php
+++ resources/views/Pages/tenders/tendersDetailes.blade.php
@@ -15,22 +15,23 @@
                                     <span class="fa fa-star"></span>
                                 </div>
 
-                                <h2 class="section-title__title"  style="direction: rtl; color:#0063ae; "  >{{ $tenders->title }}</h2>
+                                <h2 class="section-title__title" style="direction: rtl; color:#0063ae; ">
+                                    {{ $tenders->title }}</h2>
                             </div>
                             <p class="contact-page__text text-center" style="direction: rtl; ">{{ $tenders->description }}
                             </p>
 
-                            <ul class="list-unstyled contact-page__contact-list"  >
+                            <ul class="list-unstyled contact-page__contact-list">
                                 <li style="direction: rtl;">
                                     <div class="icon">
                                         <span class="fas fa-money-bill"></span>
                                     </div>
-                                    <div class="content" style="margin-right: 15px" >
+                                    <div class="content" style="margin-right: 15px">
                                         <p>قيمة العطاء :</p>
                                         <h4> {{ $tenders->value }}</h4>
                                     </div>
                                 </li>
-                                <li  style="direction: rtl;">
+                                <li style="direction: rtl;">
                                     <div class="icon" style="direction: rtl;">
                                         <span class="far fa-calendar-alt"></span>
                                     </div>
@@ -46,6 +47,7 @@
                                     <div class="content" style="margin-right: 15px">
                                         <p>تحميل الصورة :</p>
                                         <a href="{{ $tenders->preview_image }}" download>
+                                
                                             <h4>تحميل</h4>
                                         </a>
                                     </div>
@@ -59,34 +61,34 @@
                 </div>
             </div>
             <br>
-        <center >
-            <div class="row">
-                <div class="col-xl-6 col-lg-6 " style="padding: 10px">
+            <center>
+                    <div class="col-xl-6 col-lg-6 " style="padding: 10px">
 
-                    <div class="contact-page__left">
-                        <div class="contact-page__img-box">
-                            <div class="contact-page__img">
-                                <img src="{{ $tenders->preview_image }}" alt=""
-                                    download>
+                        <div class="contact-page__left">
+                            <div class="contact-page__img-box">
+                                <div class="contact-page__img">
+                                    <img src="{{ $tenders->preview_image }}" alt="" download>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
-
-                <div class="col-xl-6 col-lg-6 " style="padding: 10px">
-
-                    <div class="contact-page__left">
-                        <div class="contact-page__img-box">
-                            <div class="contact-page__img">
-                                <img src="{{ $tenders->preview_image2 }}" alt=""
-                                     download>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
             </center>
+
+            <center>
+
+                <div class="col-xl-6 col-lg-6 " style="padding: 10px">
+
+                    <div class="contact-page__left">
+                        <div class="contact-page__img-box">
+                            <div class="contact-page__img">
+                                <img src="{{ $tenders->preview_image2 }}" alt="" download>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+        </div>
+
+        </center>
         </div>
 
     </section>
